e0c6fb6b99ca04429a879da10fec330d1e4a4628
Denish-Ranpariyam <ranpariyadenish1512@gmail.com>
2022-08-06 20:05:32 +0530

    Learned about variables

--- 02variables/main.go
+++ 02variables/main.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+//first capital letter denotes that it is a public variable
+const LoginToken string = "Change me if you can!"
+
 func main() {
 	var username string = "denish"
 	fmt.Println(username)
@@ -10,4 +13,34 @@ func main() {
 	var isLoggedIn bool = false
 	fmt.Println(isLoggedIn)
 	fmt.Printf("Variable is of type: %T \n", isLoggedIn)
+
+	var smallIntValue uint8 = 255
+	fmt.Println(smallIntValue)
+	fmt.Printf("Variable is of type: %T \n", smallIntValue)
+
+	var smallFloatValue float32 = 255.32454544343435
+	fmt.Println(smallFloatValue)
+	fmt.Printf("Variable is of type: %T \n", smallFloatValue)
+
+	//default values and some aliases
+	var intValue int
+	fmt.Println(intValue)
+	fmt.Printf("Variable is of type: %T \n", intValue)
+
+	var stringValue string
+	fmt.Println(stringValue)
+	fmt.Printf("Variable is of type: %T \n", stringValue)
+
+	//implicit type
+	var website = "saymyname.com"
+	fmt.Println(website)
+	fmt.Printf("Variable is of type: %T \n", website)
+
+	//no var style
+	//this method is allowed only inside a method
+	name := "denish"
+	fmt.Println(name)
+	fmt.Printf("Variable is of type: %T \n", name)
+
+	fmt.Println("Login token: ", LoginToken)
 }
